17f8a81af1e378253b5a6709efa51ad2e5421116
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2023-12-18 11:00:06 +0700

    Doing the demo of story book

---
+++ storybook-demo/src/stories/Light.tsx
@@ -0,0 +1,20 @@
+type Props = {
+    /** some description */
+    variant: 'green' | 'yellow' | 'red';
+};
+
+/** Some comment about my Light component */
+const Light = ({ variant = 'green' }: Props) => {
+    return (
+        <div
+            style={{
+                background: variant,
+                borderRadius: '50%',
+                width: 50,
+                height: 50,
+            }}
+        ></div>
+    );
+};
+
+export default Light;

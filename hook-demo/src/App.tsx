c751cccf8512c19660904de6d50e1c39de728bad
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2023-12-20 16:33:11 +0700

    Applied useRef in Todo list exercise

--- hook-demo/src/App.tsx
+++ hook-demo/src/App.tsx
@@ -3,55 +3,55 @@ import AddTodo from './AddTodo';
 import TaskList from './TaskList';
 
 export type Todo = {
-    id: string;
-    title: string;
-    completed: boolean;
+  id: string;
+  title: string;
+  completed: boolean;
 };
 
 const App = () => {
-    const [todos, setTodos] = useState<Todo[]>(() => {
-        const localValue = localStorage.getItem('ITEMS');
-        return localValue ? JSON.parse(localValue) : [];
-    });
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    const localValue = localStorage.getItem('ITEMS');
+    return localValue ? JSON.parse(localValue) : [];
+  });
 
-    useEffect(() => {
-        localStorage.setItem('ITEMS', JSON.stringify(todos));
-    }, [todos]);
+  useEffect(() => {
+    localStorage.setItem('ITEMS', JSON.stringify(todos));
+  }, [todos]);
 
-    const addTodo = (title: string) => {
-        if (title === '') {
-            alert('Please, fill in the input if you wanna add');
-        } else {
-            setTodos([
-                ...todos,
-                { id: crypto.randomUUID(), title, completed: false },
-            ]);
-        }
-    };
+  const addTodo = (title: string) => {
+    if (title === '') {
+      alert('Please, fill in the input if you wanna add');
+    } else {
+      setTodos([
+        ...todos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]);
+    }
+  };
 
-    const deleteTodo = (id: string) =>
-        setTodos(todos.filter((todo) => todo.id !== id));
+  const deleteTodo = (id: string) =>
+    setTodos(todos.filter((todo) => todo.id !== id));
 
-    const onChangeTodo = (id: string, completed: boolean) =>
-        setTodos(
-            todos.map((todo) => {
-                if (todo.id === id) return { ...todo, completed };
-                return todo;
-            })
-        );
-
-    return (
-        <>
-            <AddTodo onSubmit={addTodo} />
-            <h1 className="header">Todo List</h1>
-            <TaskList
-                todos={todos}
-                setTodos={setTodos}
-                onDelete={deleteTodo}
-                onChangeTodo={onChangeTodo}
-            />
-        </>
+  const onChangeTodo = (id: string, completed: boolean) =>
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) return { ...todo, completed };
+        return todo;
+      })
     );
+
+  return (
+    <>
+      <AddTodo onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TaskList
+        todos={todos}
+        setTodos={setTodos}
+        onDelete={deleteTodo}
+        onChangeTodo={onChangeTodo}
+      />
+    </>
+  );
 };
 
 export default App;
--- hook-demo/src/TaskList.tsx
+++ hook-demo/src/TaskList.tsx
@@ -2,33 +2,33 @@ import { Todo } from './App';
 import Task from './Task';
 
 export type TaskListProps = {
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-    onDelete: (id: string) => void;
-    onChangeTodo: (id: string, completed: boolean) => void;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  onDelete: (id: string) => void;
+  onChangeTodo: (id: string, completed: boolean) => void;
 };
 
 const TaskList = ({
-    todos,
-    setTodos,
-    onDelete,
-    onChangeTodo,
+  todos,
+  setTodos,
+  onDelete,
+  onChangeTodo,
 }: TaskListProps) => {
-    return (
-        <ul className="list">
-            {todos.length === 0 && 'No todo available'}
-            {todos.map((todo) => (
-                <Task
-                    {...todo}
-                    key={todo.id}
-                    todos={todos}
-                    setTodos={setTodos}
-                    onDelete={onDelete}
-                    onChangeTodo={onChangeTodo}
-                />
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="list">
+      {todos.length === 0 && 'No todo available'}
+      {todos.map((todo) => (
+        <Task
+          {...todo}
+          key={todo.id}
+          todos={todos}
+          setTodos={setTodos}
+          onDelete={onDelete}
+          onChangeTodo={onChangeTodo}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default TaskList;

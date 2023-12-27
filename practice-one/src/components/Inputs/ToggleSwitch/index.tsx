35ce07a540835aaac1c4a8cc38c38bcce0ba099d
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2023-12-27 16:55:51 +0700

    #31 - Implemented the toggle switch component

---
+++ practice-one/src/components/Inputs/ToggleSwitch/index.tsx
@@ -0,0 +1,12 @@
+// SCSS
+import './ToggleSwitch.scss';
+
+type ToggleSwitchProps = {
+  isChecked?: boolean;
+};
+
+const ToggleSwitch = ({ isChecked}: ToggleSwitchProps) => {
+  return <input type="checkbox" className="toggle--switch" checked={isChecked}  />;
+};
+
+export default ToggleSwitch;

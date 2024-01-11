cddb894415e90bd0c0d7f9b8067e89664d4cf036
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-01-11 16:51:58 +0700

    #15 - Rename the SearchBar.scss

--- practice-one/src/components/Inputs/SearchBar/index.tsx
+++ practice-one/src/components/Inputs/SearchBar/index.tsx
@@ -15,7 +15,7 @@ import { Icon } from '@components/DataDisplay';
 import { TextField } from '@components/Inputs';
 
 // SCSS
-import '@components/Inputs/SearchBar/Searchbar.scss';
+import '@components/Inputs/SearchBar/searchbar.scss';
 
 type SearchBarProps = {
   label?: string;

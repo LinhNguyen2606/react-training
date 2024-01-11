db3e195aefe59f8335466e2ca94d5aefefb198dc
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-01-11 11:45:42 +0700

    #15 - Apply the code to deploy on the vercel

--- practice-one/vite.config.ts
+++ practice-one/vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import EnvironmentPlugin from 'vite-plugin-environment';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    'process.env': process.env,
-  },
-  plugins: [react(), tsconfigPaths(), EnvironmentPlugin('all', { prefix: 'REACT_' })],
-  server: {
-    port: 5173,
-  },
+  plugins: [react(), tsconfigPaths()],
   publicDir: 'src/assets',
   // Config for load variable css first
   css: {

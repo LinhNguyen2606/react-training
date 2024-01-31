a4f3ad9e76043d17204be7b772c6772326499e8c
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-01-31 09:55:04 +0700

    #176 - Apply Error Boundary into the Routes.

--- practice-two/src/App.tsx
+++ practice-two/src/App.tsx
@@ -1,30 +1,12 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-// Layouts
-import {
-  ErrorPage,
-  HomePage,
-  RolePage,
-  RulePage
-} from '@layouts';
-
-// Constant
-import { path } from '@constants';
-
+// Routes
+import { AppRoutes } from '@routes';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path={path.HOME_PATH} element={<HomePage />} />
-        <Route path={path.ROLES_PATH} element={<RolePage />} />
-        <Route path={path.RULES_PATH} element={<RulePage />} />
-        <Route path={path.ERROR_PATH} element={<ErrorPage />} />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 };

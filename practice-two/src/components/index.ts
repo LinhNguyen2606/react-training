6059ef230af0ddb5efd65118503973149e620e68
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-01-31 17:42:00 +0700

    #128 - Export the button component.

--- practice-two/src/components/index.ts
+++ practice-two/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as ErrorFallback } from '@components/ErrorFallback';
 export { default as Avatar } from '@components/Avatar';
+export { default as Button } from '@components/Button';

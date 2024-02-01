a6a3459a6ee955f445b9c0dfac2135f63a219bb1
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-01 10:06:25 +0700

    #129 - Installed the plus icon

--- practice-two/src/assets/icons/index.ts
+++ practice-two/src/assets/icons/index.ts
@@ -3,6 +3,7 @@ export { default as Clock } from '/icons/clock-regular.svg';
 export { default as Envelope } from '/icons/envelope-regular.svg';
 export { default as MagnifyingGlass } from '/icons/magnifying-glass-solid.svg';
 export { default as Pencil } from '/icons/pencil-solid.svg';
+export { default as Plus } from '/icons/plus-solid.svg';
 export { default as Upload } from '/icons/upload-solid.svg';
 export { default as UserGroup } from '/icons/user-group-solid.svg';
 export { default as Xmark } from '/icons/xmark-solid.svg';

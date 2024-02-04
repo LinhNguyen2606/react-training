11ebf8376bf642d7677672d1957f5e6826bec587
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-04 20:39:39 +0700

    #134 - Tested the search bar component with the storybook

---
+++ practice-two/src/components/SearchBar/SearchBar.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+// Component
+import { SearchBar } from '@components';
+
+export default {
+  title: 'Components/SearchBar',
+  component: SearchBar,
+  tags: ['autodocs'],
+  argTypes: {
+    label: {
+      description: 'The label of the search bar',
+      table: {
+        defaultValue: { summary: 'Users' },
+      },
+    },
+    placeholder: {
+      description: 'Search bar input placeholder',
+      table: {
+        defaultValue: { summary: 'Search' },
+      },
+    },
+    onChange: {
+      description: 'onChange event when you type the keyword in the input',
+    },
+  },
+} as Meta;
+type Story = StoryObj<typeof SearchBar>;
+
+/** Search bar is a search toolbar that helps find users quickly and easily.  */
+export const Default: Story = {
+  args: {
+    label: 'Users',
+    placeholder: 'Search',
+  },
+};

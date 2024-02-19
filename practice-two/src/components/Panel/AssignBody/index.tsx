1231b7e7646a04a5cedf09854ccceb5605a99edf
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-19 14:52:15 +0700

    #223 - Implemented the AssignBody component.

---
+++ practice-two/src/components/Panel/AssignBody/index.tsx
@@ -0,0 +1,61 @@
+import { ChangeEvent } from 'react';
+
+// Components
+import { Item, AssignmentOptions } from '@components/Panel/AssignItems';
+import AssignItemAvatarText from '@components/Panel/AssignBody/AssignItemAvatarText';
+import AssignItemTextTags from '@components/Panel/AssignBody/AssignItemTextTags';
+
+interface AssignBodyProps {
+  src?: string;
+  items: Item[];
+  isModifying: boolean;
+  selectedType: AssignmentOptions;
+  handleItemSelect: (
+    id: number
+  ) => (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const AssignBody = ({
+  src,
+  items,
+  isModifying,
+  selectedType,
+  handleItemSelect,
+}: AssignBodyProps) => {
+  
+  const style =
+    selectedType === AssignmentOptions.AssignedDirectly ? '#f4f5f9' : '#fff';
+
+  return (
+    <ul style={{ backgroundColor: style }}>
+      {items.map((item) => (
+        <li key={item.id} className="panel-assign__item">
+          {src || item.bgColor ? (
+            <AssignItemAvatarText
+              id={item.id}
+              name={item.name}
+              bgColor={item.bgColor}
+              isModifying={isModifying}
+              isAssigned={item.isAssigned}
+              handleItemSelect={handleItemSelect}
+              src={src}
+            />
+          ) : (
+            <AssignItemTextTags
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              isAssigned={item.isAssigned}
+              isModifying={isModifying}
+              assignedTo={item.assignedTo}
+              selectedType={selectedType}
+              handleItemSelect={handleItemSelect}
+            />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default AssignBody;

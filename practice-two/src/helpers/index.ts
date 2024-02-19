d3a41b119d714d0cbd577a616c7a03a6fa433e0d
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-19 14:42:13 +0700

    #221 - Implemented the EditorProfile component

--- practice-two/src/helpers/index.ts
+++ practice-two/src/helpers/index.ts
@@ -1,3 +1,4 @@
 export { generateRandomColor } from '@helpers/generate';
 export { convertFileToBase64 } from '@helpers/convert';
 export { dateFormat } from '@helpers/date';
+export { validateUsername, validateEmail } from '@helpers/validate';
---
+++ practice-two/src/interfaces/data.ts
@@ -0,0 +1,8 @@
+export interface DataItems {
+  id: number;
+  type: string;
+  label: string;
+  key: string;
+  value: string | boolean;
+  keyImageDefault?: string;
+};

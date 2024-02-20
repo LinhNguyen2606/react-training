dffb478621369f9025a7d5ecdd53c5674cc684b5
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-20 17:35:44 +0700

    #146 - Added the selectedRow in the Context.

--- practice-two/src/stores/provider.tsx
+++ practice-two/src/stores/provider.tsx
@@ -1,9 +1,15 @@
-import { ReactNode, useReducer } from 'react';
+import {
+  ReactNode,
+  useReducer,
+  useState
+} from 'react';
 
 // Stores
 import { Context, reducer } from '@stores';
-
 import { initialState } from '@stores/reducer';
+
+// Interface
+import { User } from '@interfaces';
 
 type ProviderProps = {
   children: ReactNode;
@@ -12,8 +18,22 @@ type ProviderProps = {
 const Provider: React.FC<ProviderProps> = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const [selectedRow, setSelectedRow] = useState<{
+    index: number;
+    data: User | null;
+  }>({ index: -1, data: null });
+
   return (
-    <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
+    <Context.Provider
+      value={{
+        state,
+        dispatch,
+        selectedRow,
+        setSelectedRow,
+      }}
+    >
+      {children}
+    </Context.Provider>
   );
 };
 

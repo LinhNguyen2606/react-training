1c4e3badfdbd6cadcbce25e702b48c6e9251d25a
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-20 09:03:46 +0700

    #144 - Created a config file constant.

--- practice-two/src/constants/index.ts
+++ practice-two/src/constants/index.ts
@@ -1,3 +1,4 @@
+export { API_BASE_URL, USER_API_ENDPOINT } from '@constants/config';
 export { PATH } from '@constants/path';
 export { ERROR_MESSAGE, REGEX } from '@constants/message';
 export { TYPES } from '@constants/type';

f2e44a6a310e2b9664fe2830ba01bef682c35478
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-20 17:29:05 +0700

    #146 - Created a interface ApiResponse function.

--- practice-two/src/interfaces/index.ts
+++ practice-two/src/interfaces/index.ts
@@ -4,3 +4,4 @@ export type { EnitityColumn } from '@interfaces/column';
 export type { User } from '@interfaces/user';
 export type { DataItems } from '@interfaces/data';
 export type { Item } from '@interfaces/item';
+export type { ApiResponse } from '@interfaces/respone';

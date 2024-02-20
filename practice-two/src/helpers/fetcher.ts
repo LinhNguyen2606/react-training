7125ca24ed56dc34585e63de19cf7b05e6d25607
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-20 11:12:56 +0700

    #171 - Distanced the code..

--- practice-two/src/helpers/fetcher.ts
+++ practice-two/src/helpers/fetcher.ts
@@ -6,8 +6,10 @@
  */
 export const fetcher = async <T>(url: string): Promise<T> => {
   const response = await fetch(url);
+
   if (!response.ok) {
     throw new Error('An error occurred while fetching the data.');
   }
+  
   return response.json();
 };

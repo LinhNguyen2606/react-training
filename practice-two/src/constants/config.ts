9229f6550ec939d2b91a5c9593671e1a2a13bd1d
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-02-21 15:45:55 +0700

    #236 - Updated the API of the config file in the folder constant.

--- practice-two/src/constants/config.ts
+++ practice-two/src/constants/config.ts
@@ -1,9 +1,9 @@
-/**
- * Base URL for API endpoints.
- */
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-/**
- * URL path for the users endpoint.
- */
-export const USER_API_ENDPOINT = 'users';
+export const API = {
+  BASE: import.meta.env.VITE_API_BASE_URL,
+  USER: 'users',
+  ROLE: 'roles',
+  RULE: 'rules',
+  USER_ROLES: 'userRoles',
+  USER_RULES: 'userRules',
+  ROLE_RULES: 'userRules',
+};

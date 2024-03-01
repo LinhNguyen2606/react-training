05d5ab57d0ff68c91b7d9373237a9c8cd6aafba9
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-03-01 16:16:04 +0700

    #153 - Implemented the createRole function in role service.

--- practice-two/src/services/role.ts
+++ practice-two/src/services/role.ts
@@ -18,12 +18,29 @@ import { handleAPIRequest } from '@services';
 
 export const getRoles = (): {
   data: Role[] | undefined;
+  isValidating: boolean;
 } => {
-  const { data } = useSWR<Role[]>(`${API.BASE}/${API.ROLE}`, fetcher);
+  const { data, isValidating } = useSWR<Role[]>(`${API.BASE}/${API.ROLE}`, fetcher);
   return {
     data,
+    isValidating
   };
 };
+
+/**
+ * Create a new role with the provided data.
+ * @param {User} roleData - The data for the new role.
+ * @returns {Promise<{ data: any; errMsg: string | null }>} The created user data or an error message.
+ */
+export const createRole = (roleData: Role): Promise<ApiResponse<Role>> =>
+  handleAPIRequest(`${API.BASE}/${API.ROLE}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(roleData),
+  });
+
 
 export const assignRoleToUser = (
   userId: string,

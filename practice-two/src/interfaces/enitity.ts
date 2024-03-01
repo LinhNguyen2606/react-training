f357fb6b3f200ce3cacbb3215a296452441cf81d
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-03-01 16:08:09 +0700

    #257 - Added new interface EnitityUserRoles.

--- practice-two/src/interfaces/enitity.ts
+++ practice-two/src/interfaces/enitity.ts
@@ -4,3 +4,10 @@ export interface EnitityColumn<T> {
   render?: (column: EnitityColumn<T>, item: T) => JSX.Element;
   width?: string;
 }
+
+export interface EnitityUserRoles {
+  id?: string;
+  userName?: string;
+  avatar: string;
+  bgColor: string;
+}

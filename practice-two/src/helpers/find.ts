0a25683efe8dce5604d4db187f24a7704bf11ff5
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-03-03 22:31:17 +0700

    #159 - Created findRoleIdFromAssigned helper function.

--- practice-two/src/helpers/find.ts
+++ practice-two/src/helpers/find.ts
@@ -18,3 +18,24 @@ export const findUserIdFromAssigned = <T extends { [key: string]: any }>(
 
   return userItem ? userItem.id : null;
 };
+
+/**
+ * Search for the ID of the item assigned to the user.
+ *
+ * @param roleId - Role ID.
+ * @param itemId - ID of the item to find (can be the ID of a rule or member).
+ * @param roleItems - Array containing items assigned to role.
+ * @returns Returns the item ID if found, otherwise returns null.
+ */
+export const findRoleIdFromAssigned = <T extends { [key: string]: any }>(
+  roleId: string,
+  itemId: string,
+  roleItems: T[],
+  key: string
+) => {
+  const roleItem = roleItems.find(
+    (item) => item.roleId === roleId && item[key] === itemId
+  );
+
+  return roleItem ? roleItem.id : null;
+};

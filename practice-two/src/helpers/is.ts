26eccabc1d27b71f9c44c7420703b04b9cb96e0e
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-03-03 22:30:50 +0700

    #159 - Created isItemAssignedToRole helper function.

--- practice-two/src/helpers/is.ts
+++ practice-two/src/helpers/is.ts
@@ -1,6 +1,31 @@
+/**
+ * Checks if an item is assigned to a user.
+ *
+ * @param userId - The ID of the user.
+ * @param itemId - The ID of the item.
+ * @param userItems - An array of user items.
+ * @param key - The key to compare against the item ID.
+ * @returns A boolean indicating whether the item is assigned to the user.
+ */
 export const isItemAssignedToUser = <T extends { [key: string]: any }>(
   userId: string,
   itemId: string,
   userItems: T[],
   key: string
 ) => userItems?.some((item) => item.userId === userId && item[key] === itemId);
+
+/**
+ * Checks if an item is assigned to a role.
+ *
+ * @param roleId - The ID of the role.
+ * @param itemId - The ID of the item.
+ * @param roleItems - An array of role items.
+ * @param key - The key to compare against the item ID.
+ * @returns A boolean indicating whether the item is assigned to the role.
+ */
+export const isItemAssignedToRole = <T extends { [key: string]: any }>(
+  roleId: string,
+  itemId: string,
+  roleItems: T[],
+  key: string
+) => roleItems?.some((item) => item.roleId === roleId && item[key] === itemId);

b2dafec2b9222c973cfa0c6165091abc1294058c
LinhNguyen2606 <vinhnguyen.280468@gmail.com>
2024-03-11 15:26:36 +0700

    #171 - Added the selectedRow and dataItems into the reducer.

--- practice-two/src/stores/reducer.tsx
+++ practice-two/src/stores/reducer.tsx
@@ -1,29 +1,38 @@
 // Constant
 import { TYPES } from '@constants';
+import { DataItems } from '@interfaces';
 
-export interface State {
+export interface State<T> {
   toast: 'idle' | 'processing' | 'success' | 'failure';
+  selectedRow: { index: number; data: T | null };
+  dataItems: DataItems[];
 }
 
-export interface Action<T> {
+export interface Action {
   type: keyof typeof TYPES;
-  payload?: T;
+  payload?: any;
 }
 
-export const initialState: State = {
+export const initialState: State<any> = {
   toast: 'idle',
+  selectedRow: { index: -1, data: null },
+  dataItems: [],
 };
 
-const Reducer = <T,>(state: State, action: Action<T>): State => {
+const Reducer = <T,>(state: State<T>, action: Action): State<T> => {
   switch (action.type) {
+    case TYPES.IDLE:
+      return { ...state, toast: 'idle' };
     case TYPES.PROCESSING:
       return { ...state, toast: 'processing' };
     case TYPES.SUCCESS:
       return { ...state, toast: 'success' };
     case TYPES.FAILURE:
       return { ...state, toast: 'failure' };
-    case TYPES.IDLE:
-      return { ...state, toast: 'idle' };
+    case TYPES.SELECTED_ROW:
+      return { ...state, selectedRow: action.payload };
+    case TYPES.DATA_ITEMS:
+      return { ...state, dataItems: action.payload };
     default:
       throw new Error('Invalid action');
   }
